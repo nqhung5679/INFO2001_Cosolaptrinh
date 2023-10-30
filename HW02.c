91e1dbc54ae26fdc8765bc02cc386b8967a332f1
nqhung5679 <BIT230188@st.cmc-u.edu.vn>
2023-10-30 16:53:47 +0700

    HW

--- HW02.c
+++ HW02.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
+
 int main(){
-	int a,b;
-	printf("a, b (b!=0, -10^18<=a,b<=10^18):", a, b);
+	int a, b;
+	printf("Enter a and b (b != 0, -10^18 <= a, b <= 10^18): ");
 	scanf("%d %d", &a, &b);
 	
-	int c,d;
-	c = a / b;
-	d = a % b;
-	printf("Phan Nguyen Phan du: %d %d", c,d);
+	int c = a / b;
+	int d = a % b;
+	printf("Phan Nguyen: %d\n", c);
+	printf("Phan du: %d\n", d);
+	
 	return 0;
 }
